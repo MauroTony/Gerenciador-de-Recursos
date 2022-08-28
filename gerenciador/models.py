ff056f99b35e6a531d26b50fa3f1a2913f1818ea
Mauro Tony <57079165+MauroTony@users.noreply.github.com>
2022-08-28 10:17:27 -0300

    Adicionando Models

--- gerenciador/models.py
+++ gerenciador/models.py
@@ -1,2 +1,31 @@
-from datetime import datetime
 from django.db import models
+
+
+class Usuario(models.Model):
+    id = models.AutoField(primary_key=True)
+    username = models.CharField(max_length=100, unique=True)
+    password = models.CharField(max_length=100)
+    is_admin = models.BooleanField(default=False)
+
+    class Meta:
+        db_table = "users"
+
+
+class Resources(models.Model):
+    id = models.AutoField(primary_key=True)
+    description = models.IntegerField(unique=True)
+    available = models.BooleanField(default=True)
+
+    class Meta:
+        db_table = "resources"
+
+
+class ResourceScheduling(models.Model):
+    id = models.AutoField(primary_key=True)
+    resource = models.ForeignKey(Resources, on_delete=models.DO_NOTHING)
+    initial_date = models.DateTimeField()
+    final_date = models.DateTimeField()
+    user = models.ForeignKey(Usuario, on_delete=models.DO_NOTHING)
+
+    class Meta:
+        db_table = "resource_scheduling"
